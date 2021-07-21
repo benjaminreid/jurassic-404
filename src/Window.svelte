00646abfb78daf21d9f7a71ea9ad94a50ac61e67
Benjamin Reid <benjaminreid.nouveller@gmail.com>
2021-07-21 16:22:32 +0100

    Draggable window

--- src/Window.svelte
+++ src/Window.svelte
@@ -2,14 +2,38 @@
   export let name;
   export let width;
   export let height;
+  export let left = 0;
+  export let top = 0;
+
+  let moving = false;
+
+  function onMouseDown() {
+    moving = true;
+  }
+
+  function onMouseMove(e) {
+    if (moving) {
+      left += e.movementX;
+      top += e.movementY;
+    }
+  }
+
+  function onMouseUp() {
+    moving = false;
+  }
 </script>
 
-<div class="container" style="width: {width}px; height: {height}px;">
-  <header>
+<div
+  class="container"
+  style="width: {width}px; height: {height}px; left: {left}px; top: {top}px;"
+>
+  <header on:mousedown={onMouseDown}>
     {name}
   </header>
   <slot />
 </div>
+
+<svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
 
 <style>
   .container {
@@ -17,9 +41,12 @@
     border: solid 3px var(--grey);
     display: flex;
     flex-direction: column;
+    position: absolute;
   }
 
   header {
     background-color: var(--grey);
+    cursor: move;
+    user-select: none;
   }
 </style>
