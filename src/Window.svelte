a6f8342811a0afe1840f99892b513cc8afbdb2b6
Benjamin Reid <benjaminreid.nouveller@gmail.com>
2021-07-21 16:32:36 +0100

    Style console

--- src/Window.svelte
+++ src/Window.svelte
@@ -38,15 +38,44 @@
 <style>
   .container {
     --grey: #cccccc;
-    border: solid 3px var(--grey);
+    border: solid 5px var(--grey);
+    box-shadow: 0 0 1px #000, 1px 1px 2px #000;
     display: flex;
     flex-direction: column;
     position: absolute;
+  }
+
+  .container::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    border: solid 1px rgba(0, 0, 0, 0.8);
+    pointer-events: none;
+  }
+
+  .container::after {
+    content: "";
+    position: absolute;
+    top: -5px;
+    left: -5px;
+    right: -5px;
+    bottom: -5px;
+    border: solid 1px rgba(255, 255, 255, 0.8);
+    pointer-events: none;
   }
 
   header {
     background-color: var(--grey);
     cursor: move;
     user-select: none;
+    padding: 4px 8px;
+    font-family: "MonoLisa", "Courier", sans-serif;
+    font-size: 13px;
+    font-weight: bold;
+    box-shadow: inset 0 2px 1px rgba(255, 255, 255, 0.8);
+    border-bottom: solid 1px black;
   }
 </style>
