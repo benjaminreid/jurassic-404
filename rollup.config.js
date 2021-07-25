120a8addc78f32c1424163a1a6216e670330d38b
Benjamin Reid <benjaminreid.nouveller@gmail.com>
2021-07-25 11:48:59 +0100

    Stop CSS emitting

--- rollup.config.js
+++ rollup.config.js
@@ -46,6 +46,7 @@ export default {
         // enable run-time checks when not in production
         dev: !production,
       },
+      emitCss: false,
     }),
     // we'll extract any component CSS out into
     // a separate file - better for performance
